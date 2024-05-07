74fa444cfe3c0218c0b75dc3dc07748ac2fe3264
manoj <pitlamanoj1@gmail.com>
2024-05-07 14:24:54 +0530

    cors updated

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ const app = express()
 
 
 app.use(cors({
-    origin: 'https://playtube-mern-app-ivory.vercel.app',
-    methods: ['GET', 'POST', 'DELETE','PATCH'],
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
     credentials: true
-}))
+}));
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
