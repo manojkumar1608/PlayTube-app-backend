7b6288b40a253404eedcb7ccfaed0b1199a36bc6
manoj <pitlamanoj1@gmail.com>
2024-05-08 16:39:03 +0530

    added option in res.cookie

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -145,7 +145,7 @@ const loginUser = asyncHandler(async (req, res) =>{
     const options = {
         httpOnly: true,
         secure: true,
-        sameSite: 'none'
+        sameSite: 'None',
     }
 
     return res
